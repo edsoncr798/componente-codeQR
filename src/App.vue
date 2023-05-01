ee713688eb61e4adba3241603aafb1c9101ce821
edson andi <edsoncr798@gmail.com>
2023-05-01 08:31:06 -0500

    change tags section a main

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <section class="w-full h-screen flex bg-[#d5e1ef]">
+  <main class="w-full h-screen flex bg-[#d5e1ef]">
     <div class=" border-none shadow-2xl rounded-[15px] w-[280px] m-auto bg-[#ffffff]">
 
       <div class="p-[15px]">
@@ -10,14 +10,14 @@
 
       <div class="px-[25px] mb-[30px]">
         <h1 class="font-bold text-[19px] text-center text-[#1f3251]">Improve your front-end skills by building projects</h1>
-        <p class="pt-[5px] text-center text-[12px] font-normal text-[#d6e2f0]">
+        <p class="pt-[5px] font-[outfit] text-center text-[12px] font-normal text-[#d6e2f0]">
           Scan the QR code to visit Frontend Mentor and take your coding skills
           to the next level
         </p>
       </div>
 
     </div>
-  </section>
+  </main>
 </template>
 
 <style scoped>
